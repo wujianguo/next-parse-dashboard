49a1320b73c91c524e2ad3bacf9bda9a9dd2af2b
wujianguo <lsjustin89@gmail.com>
2023-07-25 13:24:39 +0800

    docs: add license and change template name to next-parse-dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/apps',
+        permanent: false,
+      }, {
+        source: '/apps/:app',
+        destination: '/apps/:app/overview',
+        permanent: false,
+      }
+    ];
+  },
 }
 
 export default nextConfig
